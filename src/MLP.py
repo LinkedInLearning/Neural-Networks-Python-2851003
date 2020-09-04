ee2eddd23637ad9000c3ada31c90f0d8c8bddbef
Eduardo Corpeño <kuashio@gmail.com>
2020-09-04 14:17:20 -0600

    RECORDED 04_07 SUCCESSFULLY

--- src/MLP.py
+++ src/MLP.py
@@ -33,23 +33,28 @@ class MultiLayerPerceptron:
           bias:    The bias term. The same bias is used for all neurons.
           eta:     The learning rate."""
 
-    def __init__(self, layers, bias = 1.0):
+    def __init__(self, layers, bias = 1.0, eta = 0.5):
         """Return a new MLP object with the specified parameters.""" 
         self.layers = np.array(layers,dtype=object)
         self.bias = bias
+        self.eta = eta
         self.network = [] # The list of lists of neurons
         self.values = []  # The list of lists of output values
+        self.d = []       # The list of lists of error terms (lowercase deltas)
 
         for i in range(len(self.layers)):
             self.values.append([])
+            self.d.append([])
             self.network.append([])
             self.values[i] = [0.0 for j in range(self.layers[i])]
+            self.d[i] = [0.0 for j in range(self.layers[i])]
             if i > 0:      #network[0] is the input layer, so it has no neurons
                 for j in range(self.layers[i]): 
                     self.network[i].append(Perceptron(inputs = self.layers[i-1], bias = self.bias))
         
         self.network = np.array([np.array(x) for x in self.network],dtype=object)
         self.values = np.array([np.array(x) for x in self.values],dtype=object)
+        self.d = np.array([np.array(x) for x in self.d],dtype=object)
 
     def set_weights(self, w_init):
         """Set the weights. 
@@ -73,18 +78,55 @@ class MultiLayerPerceptron:
             for j in range(self.layers[i]):  
                 self.values[i][j] = self.network[i][j].run(self.values[i-1])
         return self.values[-1]
+    
+    def bp(self, x, y):
+        """Run a single (x,y) pair with the backpropagation algorithm."""
+        x = np.array(x,dtype=object)
+        y = np.array(y,dtype=object)
+
+        # Challenge: Write the Backpropagation Algorithm. 
+        # Here you have it step by step:
+
+        # STEP 1: Feed a sample to the network 
+        
+        # STEP 2: Calculate the MSE
+
+        # STEP 3: Calculate the output error terms
+
+        # STEP 4: Calculate the error term of each unit on each layer
+        for i in reversed(range(1,len(self.network)-1)):
+            for h in range(len(self.network[i])):
+                fwd_error = 0.0
+                for k in range(self.layers[i+1]): 
+                    fwd_error += # fill in the blank               
+                self.d[i][h] = # fill in the blank
+
+        # STEPS 5 & 6: Calculate the deltas and update the weights
+        for i in range(1,len(self.network)):
+            for j in range(self.layers[i]):
+                for k in range(self.layers[i-1]+1):
+                    # fill in the blank
+        return MSE
 
 
 
 #test code
-mlp = MultiLayerPerceptron(layers=[2,2,1])  #mlp
-mlp.set_weights([[[-10,-10,15],[15,15,-10]],[[10,10,-15]]])
+mlp = MultiLayerPerceptron(layers=[2,2,1])
+print("\nTraining Neural Network as an XOR Gate...\n")
+for i in range(3000):
+    MSE = 0.0
+    MSE += mlp.bp([0,0],[0])
+    MSE += mlp.bp([0,1],[1])
+    MSE += mlp.bp([1,0],[1])
+    MSE += mlp.bp([1,1],[0])
+    MSE = MSE / 4
+    if(i%100 == 0):
+        print (MSE)
+
 mlp.printWeights()
+    
 print("MLP:")
 print ("0 0 = {0:.10f}".format(mlp.run([0,0])[0]))
 print ("0 1 = {0:.10f}".format(mlp.run([0,1])[0]))
 print ("1 0 = {0:.10f}".format(mlp.run([1,0])[0]))
 print ("1 1 = {0:.10f}".format(mlp.run([1,1])[0]))
-
-
-
