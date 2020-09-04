edf24153835a47839c313487f775e078b7a28d96
Eduardo Corpeño <kuashio@gmail.com>
2020-09-04 14:12:32 -0600

    RECORDED 02_09 SUCCESSFULLY

--- src/MLP.py
+++ src/MLP.py
@@ -27,14 +27,10 @@ class Perceptron:
 
 #test code
 neuron = Perceptron(inputs=2)
-neuron.set_weights([10,10,-15]) #AND
-
-# Challenge: Write your OR weights
+neuron.set_weights([15,15,-10]) #OR
 
 print("Gate:")
 print ("0 0 = {0:.10f}".format(neuron.run([0,0])))
 print ("0 1 = {0:.10f}".format(neuron.run([0,1])))
 print ("1 0 = {0:.10f}".format(neuron.run([1,0])))
 print ("1 1 = {0:.10f}".format(neuron.run([1,1])))
-
-
